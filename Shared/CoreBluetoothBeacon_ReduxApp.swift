59ff734672974b90cc56e563a5d05c21c71cae57
Nick <npvisual@users.noreply.github.com>
2020-10-05 12:41:25 -0500

    Initial Commit

---
+++ Shared/CoreBluetoothBeacon_ReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreBluetoothBeacon_ReduxApp.swift
+//  Shared
+//
+//  Created by Nicolas Philippe on 10/5/20.
+//
+
+import SwiftUI
+
+@main
+struct CoreBluetoothBeacon_ReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
